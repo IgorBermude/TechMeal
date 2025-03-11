460758034043a887971b844fe99539b93509f9f4
igors <@igorbermude>
2025-03-11 17:01:29 -0300

    CRUD Produto

--- src/main/java/br/bom/techmeal/academic/entity/Produto.java
+++ src/main/java/br/bom/techmeal/academic/entity/Produto.java
@@ -1,6 +1,9 @@
 package br.bom.techmeal.academic.entity;
 
+import br.bom.techmeal.academic.dto.ProdutoDTO;
+import br.bom.techmeal.academic.dto.UsuarioDTO;
 import jakarta.persistence.*;
+import org.springframework.beans.BeanUtils;
 
 import java.io.Serializable;
 import java.util.List;
@@ -31,6 +34,25 @@ public class Produto implements Serializable {
 
     @ManyToMany(mappedBy = "produtoListComanda", fetch = FetchType.LAZY)
     private List<Comanda> comandaListProduto;
+
+    public Produto(int idProduto, String nomeProduto, long codigoBarrasProduto, int quantProduto, double precoProduto, double valorDeCustoProduto, List<HistoricoPreco> historicoPrecoList, List<Comanda> comandaListProduto) {
+        this.idProduto = idProduto;
+        this.nomeProduto = nomeProduto;
+        this.codigoBarrasProduto = codigoBarrasProduto;
+        this.quantProduto = quantProduto;
+        this.precoProduto = precoProduto;
+        this.valorDeCustoProduto = valorDeCustoProduto;
+        this.historicoPrecoList = historicoPrecoList;
+        this.comandaListProduto = comandaListProduto;
+    }
+
+    public Produto(ProdutoDTO produto){
+        BeanUtils.copyProperties(produto, this);
+    }
+
+    public Produto(){
+
+    }
 
     public void gerarCodigoBarras() {
         // implementação do código
